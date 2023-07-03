e19ece5d67b9b364c0abfe600f2a4d583aa828fe
dcampbell <dcampbell7095@gmail.com>
2023-07-02 21:40:11 -0400

    adding base code for successfull authentication and data pulls

--- Oauth.py
+++ Oauth.py
@@ -1,18 +1,21 @@
 from config import config
 import tweepy
-from tweepy import OAuthHandler
+import tweepy
+import logging
 
 consumer_key = config["API_KEY"]
 consumer_secret = config["CONSUMER_SECRET"]
 access_token = config["ACCESS_TOKEN"]
 access_secret = config["ACCESS_SECRET"]
+bearer_token = config["BEARER_TOKEN"]
+client_id = config["CLIENT_ID"]
+client_secret = config["CLIENT_SECRET"]
 
 print(consumer_key)
+print(consumer_secret)
 
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
+client = tweepy.Client(bearer_token, consumer_key, consumer_secret, access_token, access_secret)
 
-api = tweepy.API(auth)
+logging.basicConfig(level=logging.DEBUG)
 
-for status in tweepy.Cursor(api.home_timeline).items(10):
-    print(status.text)
+client.get_users_tweets(id="469761474", max_results = 10, user_auth = True)
